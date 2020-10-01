c6ee5be5bd09dc6057d57dae329e6385e1ccb3f6
sbastidas37@misena.edu.co <sbastidas37@misena.edu.co>
2020-09-30 22:56:06 -0500

    Solicita el nombre del usuario

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace Laboratorio03GitHub
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            /* Solicito el nombre del usuario */
+            Console.WriteLine("\nWhat is your name? ");
         }
     }
 }
