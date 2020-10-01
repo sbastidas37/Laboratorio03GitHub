372e00c1af273132afc5ddb5357326a44598a40a
sbastidas37@misena.edu.co <sbastidas37@misena.edu.co>
2020-09-30 22:57:52 -0500

    Capturo los datos por pantalla

--- Program.cs
+++ Program.cs
@@ -9,6 +9,11 @@ namespace Laboratorio03GitHub
             Console.WriteLine("Hello World!");
             /* Solicito el nombre del usuario */
             Console.WriteLine("\nWhat is your name? ");
+            /*Capturo los datos (nombre y fecha) en unas variables*/
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+
+            Console.WriteLine("Su nombre es " + name + " la fecha de hoy es " + date);
         }
     }
 }
